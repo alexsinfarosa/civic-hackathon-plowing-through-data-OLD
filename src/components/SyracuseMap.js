9ebe2a43bf63faf7fd166c1e519386af4ff448b9
Alex Sinfarosa <alexsinfarosa@users.noreply.github.com>
2022-01-26 17:12:36 +0100

    update tilelayer url

--- src/components/SyracuseMap.js
+++ src/components/SyracuseMap.js
@@ -1,54 +1,47 @@
-import React, { Component } from "react";
-import { inject, observer } from "mobx-react";
+import { inject, observer } from 'mobx-react'
+import React, { Component } from 'react'
+import { GeoJSON, Map, Popup, TileLayer } from 'react-leaflet'
 
-import {
-  Map,
-  TileLayer,
-  //   Marker,
-  Popup,
-  GeoJSON
-} from "react-leaflet";
-
-@inject("app")
+@inject('app')
 @observer
 export default class SyracuseMap extends Component {
   constructor() {
-    super();
+    super()
     this.state = {
       lat: 43.035,
       lng: -76.14,
-      zoom: 12.5
-    };
+      zoom: 12.5,
+    }
   }
 
   componentDidMount() {
-    const leafletMap = this.leafletMap.leafletElement;
-    leafletMap.on("zoomend", () => {
-      console.log("Current zoom level -> ", leafletMap.getZoom());
-    });
+    const leafletMap = this.leafletMap.leafletElement
+    leafletMap.on('zoomend', () => {
+      console.log('Current zoom level -> ', leafletMap.getZoom())
+    })
   }
 
   render() {
-    const position = [this.state.lat, this.state.lng];
-    const { allData } = this.props.app;
+    const position = [this.state.lat, this.state.lng]
+    const { allData } = this.props.app
 
-    const RenderGeojson = d => {
+    const RenderGeojson = (d) => {
       return allData.map((obj, i) => {
-        const { properties } = obj;
-        const lapsedTime = properties.LT;
-        let style = { color: "" };
+        const { properties } = obj
+        const lapsedTime = properties.LT
+        let style = { color: '' }
 
         // console.log(lineColor[i]);
         if (lapsedTime >= 0 && lapsedTime <= 12) {
-          style = { color: "green", opacity: 0.7 };
+          style = { color: 'green', opacity: 0.7 }
         }
 
         if (lapsedTime > 12 && lapsedTime <= 24) {
-          style = { color: "orange", opacity: 0.7 };
+          style = { color: 'orange', opacity: 0.7 }
         }
 
         if (lapsedTime > 24) {
-          style = { color: "red", opacity: 0.7 };
+          style = { color: 'red', opacity: 0.7 }
         }
 
         return (
@@ -57,21 +50,23 @@ export default class SyracuseMap extends Component {
               <span>{properties.FULLNAME}</span>
             </Popup>
           </GeoJSON>
-        );
-      });
-    };
+        )
+      })
+    }
 
     return (
       <Map
-        style={{ width: "100%", height: "100%" }}
+        style={{
+          width: '100%',
+          height: '100%',
+        }}
         center={position}
         zoom={this.state.zoom}
-        ref={m => (this.leafletMap = m)}
+        ref={(m) => (this.leafletMap = m)}
       >
-        <TileLayer url="http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png" />
-
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
         <RenderGeojson />
       </Map>
-    );
+    )
   }
 }
